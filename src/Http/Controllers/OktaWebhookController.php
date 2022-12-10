bf6d2e171d1f3baac9debc8c66fd8ab55e0d0ed7
Carlos Carretta <carretta2@gmail.com>
2022-12-10 15:26:00 -0500

    add webhook handling

---
+++ src/Http/Controllers/OktaWebhookController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Pochocho\OktaSamlSso\Http\Controllers;
+
+use Pochocho\OktaSamlSso\Events\OktaWebhookReceived;
+use Pochocho\OktaSamlSso\Http\Requests\OktaWebhookRequest;
+
+class OktaWebhookController {
+
+
+    public function oktaVerification(OktaWebhookRequest $request){
+
+        if($request->hasHeader('x-okta-verification-challenge')){
+            return response()->json(
+                [
+                    'verification' => $request->header('x-okta-verification-challenge'),
+                ]
+            );
+        }
+
+    }
+
+    public function processWebhook(OktaWebhookRequest $request){
+
+        $eventMeta = $request->except('data');
+        $eventData = $request->only('data');
+
+        if(isset($eventData['data'])){
+            event(new OktaWebhookReceived($eventMeta, $eventData['data']));
+        }
+    }
+
+}
